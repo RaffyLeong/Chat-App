149865313ed82d5381da848eda7c2a5b3aa7611e
Raffy <raffy36249@gmail.com>
2024-11-18 21:13:34 +0800

    3-getMyConversations query added

--- convex/schema.ts
+++ convex/schema.ts
@@ -9,12 +9,22 @@ export default defineSchema({
         tokenIdentifier: v.string(),
         isOnline: v.boolean(),
     }).index("by_tokenIdentifier", ["tokenIdentifier"]),
-    
+
+
+    // Conversations
     conversations: defineTable({
         participants: v.array(v.id("users")),
         isGroup: v.boolean(),
         groupName: v.optional(v.string()),
         groupImage: v.optional(v.string()),
         admin: v.optional(v.id("users")),
-    })
+    }),
+
+    // Messages
+    messages: defineTable({
+        conversation: v.id("conversations"),
+        sender: v.string(), // should be string so that it doesnt throw error in openai part("ChatGPT")
+        content: v.string(),
+        messageType: v.union(v.literal("text"),v.literal("image"),v.literal("video")),
+    }).index("by_conversation", ["conversation"]),
 });
